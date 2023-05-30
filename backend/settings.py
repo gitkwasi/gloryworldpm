f7b63278cb2c5317abeb2c569e02e141a3632ef3
Siaw Gideon <gitkwasi@gmail.com>
2023-05-30 18:31:01 +0000

    4th commit

--- backend/settings.py
+++ backend/settings.py
@@ -141,10 +141,22 @@ AUTH_USER_MODEL = 'api.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER':'postgres',
+        'PASSWORD':'ktPzJnlfL35bUkzDPdoO',
+        'HOST':'containers-us-west-150.railway.app',
+        'PORT':'6610',
     }
 }
+
+
+# {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
